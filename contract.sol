2708253b7a7b551877ea5c190410e796234fdcaa
Hafsa Ahmad <hafsa.ahmad043@gmail.com>
2025-09-24 22:41:35 +0500

    Update contract.sol
    edited

--- contract.sol
+++ contract.sol
@@ -1,83 +1,206 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.28;
 
+
 contract MultiSigWallet {
-    /* ========== STATE VARIABLES ========== */
+    /* ========== STATE ========== */
+
     address[] public owners;
     mapping(address => bool) public isOwner;
-    uint public required;
+    uint256 public required; // number of confirmations required
 
     struct Transaction {
         address to;
-        uint value;
+        uint256 value;
         bytes data;
         bool executed;
+        uint256 numConfirmations;
     }
 
+    // txId => owner => bool
+    mapping(uint256 => mapping(address => bool)) public isConfirmed;
     Transaction[] public transactions;
-    mapping(uint => mapping(address => bool)) public confirmed;
 
     /* ========== EVENTS ========== */
-    event Submit(uint indexed txId, address indexed to, uint value, bytes data);
-    event Confirm(address indexed owner, uint indexed txId);
-    event Revoke(address indexed owner, uint indexed txId);
-    event Execute(uint indexed txId);
+
+    event Deposit(address indexed sender, uint256 amount, uint256 balance);
+    event SubmitTransaction(
+        address indexed owner,
+        uint256 indexed txId,
+        address indexed to,
+        uint256 value,
+        bytes data
+    );
+    event ConfirmTransaction(address indexed owner, uint256 indexed txId);
+    event RevokeConfirmation(address indexed owner, uint256 indexed txId);
+    event ExecuteTransaction(address indexed owner, uint256 indexed txId);
 
     /* ========== MODIFIERS ========== */
+
     modifier onlyOwner() {
+        require(isOwner[msg.sender], "MultiSig: not owner");
         _;
     }
 
-    modifier txExists(uint _txId) {
+    modifier txExists(uint256 _txId) {
+        require(_txId < transactions.length, "MultiSig: tx does not exist");
         _;
     }
 
-    modifier notExecuted(uint _txId) {
+    modifier notExecuted(uint256 _txId) {
+        require(!transactions[_txId].executed, "MultiSig: tx already executed");
         _;
     }
 
-    modifier notConfirmed(uint _txId) {
+    modifier notConfirmed(uint256 _txId) {
+        require(!isConfirmed[_txId][msg.sender], "MultiSig: tx already confirmed");
         _;
     }
 
     /* ========== CONSTRUCTOR ========== */
-    constructor(address[] memory _owners, uint _required) {
-        // set owners and required confirmations
+
+    /// @notice Create a multisig wallet
+    /// @param _owners list of initial owners
+    /// @param _required number of confirmations required for execution
+    constructor(address[] memory _owners, uint256 _required) {
+        require(_owners.length > 0, "MultiSig: owners required");
+        require(
+            _required > 0 && _required <= _owners.length,
+            "MultiSig: invalid required number"
+        );
+
+        for (uint256 i = 0; i < _owners.length; i++) {
+            address owner = _owners[i];
+
+            require(owner != address(0), "MultiSig: invalid owner");
+            require(!isOwner[owner], "MultiSig: owner not unique");
+
+            isOwner[owner] = true;
+            owners.push(owner);
+        }
+
+        required = _required;
     }
 
-    /* ========== FUNCTIONS ========== */
-    function submitTransaction(address _to, uint _value, bytes memory _data) 
-        public 
-        onlyOwner 
-    {
-        //  store new transaction
+    /* ========== RECEIVE / FALLBACK ========== */
+
+    receive() external payable {
+        emit Deposit(msg.sender, msg.value, address(this).balance);
     }
 
-    function confirmTransaction(uint _txId) 
-        public 
-        onlyOwner 
-        txExists(_txId) 
-        notExecuted(_txId) 
-        notConfirmed(_txId) 
-    {
-        //  mark confirmation
+    /* ========== EXTERNAL FUNCTIONS ========== */
+
+    /// @notice Submit a transaction proposal to the wallet
+    /// @param _to recipient address
+    /// @param _value amount of wei to send
+    /// @param _data calldata payload (optional)
+    /// @return txId the id of the submitted transaction
+    function submitTransaction(
+        address _to,
+        uint256 _value,
+        bytes calldata _data
+    ) external onlyOwner returns (uint256 txId) {
+        txId = transactions.length;
+
+        transactions.push(
+            Transaction({
+                to: _to,
+                value: _value,
+                data: _data,
+                executed: false,
+                numConfirmations: 0
+            })
+        );
+
+        emit SubmitTransaction(msg.sender, txId, _to, _value, _data);
     }
 
-    function executeTransaction(uint _txId) 
-        public 
-        onlyOwner 
-        txExists(_txId) 
-        notExecuted(_txId) 
+    /// @notice Confirm a transaction
+    /// @param _txId transaction id
+    function confirmTransaction(uint256 _txId)
+        external
+        onlyOwner
+        txExists(_txId)
+        notExecuted(_txId)
+        notConfirmed(_txId)
     {
-        // TODO: 
+        Transaction storage transaction = transactions[_txId];
+        transaction.numConfirmations += 1;
+        isConfirmed[_txId][msg.sender] = true;
+
+        emit ConfirmTransaction(msg.sender, _txId);
     }
 
-    function revokeConfirmation(uint _txId) 
-        public 
-        onlyOwner 
-        txExists(_txId) 
-        notExecuted(_txId) 
+    /// @notice Revoke a prior confirmation
+    /// @param _txId transaction id
+    function revokeConfirmation(uint256 _txId)
+        external
+        onlyOwner
+        txExists(_txId)
+        notExecuted(_txId)
     {
-        
+        require(isConfirmed[_txId][msg.sender], "MultiSig: tx not confirmed");
+
+        Transaction storage transaction = transactions[_txId];
+        transaction.numConfirmations -= 1;
+        isConfirmed[_txId][msg.sender] = false;
+
+        emit RevokeConfirmation(msg.sender, _txId);
+    }
+
+    /// @notice Execute a confirmed transaction
+    /// @param _txId transaction id
+    function executeTransaction(uint256 _txId)
+        external
+        onlyOwner
+        txExists(_txId)
+        notExecuted(_txId)
+    {
+        Transaction storage transaction = transactions[_txId];
+        require(
+            transaction.numConfirmations >= required,
+            "MultiSig: insufficient confirmations"
+        );
+
+        transaction.executed = true;
+
+        // solhint-disable-next-line avoid-low-level-calls
+        (bool success, ) = transaction.to.call{value: transaction.value}(
+            transaction.data
+        );
+        require(success, "MultiSig: tx failed");
+
+        emit ExecuteTransaction(msg.sender, _txId);
+    }
+
+    /* ========== VIEW HELPERS ========== */
+
+    function getOwners() external view returns (address[] memory) {
+        return owners;
+    }
+
+    function getTransactionCount() external view returns (uint256) {
+        return transactions.length;
+    }
+
+    function getTransaction(uint256 _txId)
+        external
+        view
+        returns (
+            address to,
+            uint256 value,
+            bytes memory data,
+            bool executed,
+            uint256 numConfirmations
+        )
+    {
+        Transaction storage transaction = transactions[_txId];
+        return (
+            transaction.to,
+            transaction.value,
+            transaction.data,
+            transaction.executed,
+            transaction.numConfirmations
+        );
     }
 }
